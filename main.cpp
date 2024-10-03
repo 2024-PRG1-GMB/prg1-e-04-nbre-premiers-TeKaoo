b355cb55066c34881d3193caa52b4bbda1118e1e
TeKao <theo.kilcher@hes-so.ch>
2024-10-03 22:27:46 +0200

    Mise en place de l'environnement de travail. Initialisation de la demande d'un nombre à l'utilisateur et préparation du tableau pour retourner les valeurs. Quelques essais pour tester et lister les nombres premiers, pas de résultats.

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "Ce programme ... " << endl << endl << endl;
+
+    int nb;
+    do
+    {
+        cout << "Entrer un nombre [2 - 1000] :  " ;
+        cin >> nb;
+    } while (nb < 2 || nb > 1000);
+
+    //int nb_testant = nb;
+    //int result [nb];
+    //for (int j = nb; j > 0;) // vient faire tourner la boucle pour J vaut nb rentré jusqu'à ce que nb vale 0
+    //{
+    //    int i = 2;
+    //    do
+    //    {
+    //        //for (; i <= sqrt(j); i++)
+    //        //{
+    //        //}
+    //        j--;
+    //        // result[j] = {j};
+    //    } while (j % i++ == 0);
+    //    cout << j << " : "  << j << " % " << i << " = " << j % i << endl;
+    //}
+
+    //for (int ligne = 'a'; ligne < 'e'; ligne++)
+    //{
+    //    for (int col = nb; (col / 5) > 1; col--)
+    //    {
+    //        int x = nb;
+    //        cout << result[x] << ' ';
+    //        --x;
+    //    }
+    //    cout << endl;
+    //}
+
+    int result[15] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
+    for (int i = 0; i < nb;)
+    {
+        cout << "       ";
+        for (int j = 0; j < 5; i++, j++)
+        {
+            cout << result [i] << "     ";
+        }
+        cout << endl;
+
+    }
+}
+
+
+
